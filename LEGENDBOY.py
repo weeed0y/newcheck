ea32bfafd77a6a86a2846dbd7c96ddc1c495b2f4
Kk <94806103+weeed0y@users.noreply.github.com>
2022-02-09 03:46:03 +0530

    Update LEGENDBOY.py

--- LEGENDBOY.py
+++ LEGENDBOY.py
@@ -13,10 +13,10 @@ except:
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("BOT_TOKEN", None)
-lnbot = TelegramClient("legendboy", API_ID, API_HASH).start(bot_token=token)
+lnbot = TelegramClient("kk_legend", API_ID, API_HASH).start(bot_token=token)
 
 
-class LegendBoy(TelegramClient):
+class kk_legend(TelegramClient):
     def __init__(self, string, api_id, api_hash):
         super().__init__(StringSession(string), api_id, api_hash)
         self.id = None
@@ -46,8 +46,8 @@ Your heroku bot username is {self.heroku_username}"""
         return detail
 
 
-if Config.LEGEND_STRING:
-    session_name = str(Config.LEGEND_STRING)
+if Config.kk_legend_STRING:
+    session_name = str(Config.kk_legend_STRING)
     sweetie = LegendBoy(session_name, Config.APP_ID, Config.API_HASH)
 else:
     session_name = "startup"
